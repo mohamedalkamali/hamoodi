c5005f920f38c5082ddda02dacb676cca70ea275
Mohamed Alkamali <74471880+mohamedalkamali@users.noreply.github.com>
2020-11-28 22:44:40 +0200

    Add files via upload

---
+++ index20.php
@@ -0,0 +1,74 @@
+<?php
+?>
+<! DOCTYPE html>
+<html>
+<head>
+         
+    <meta charset="utf-8">
+    <meta name="viewpor" content=" width=device-width, initial-scale=1.8">
+
+    <title>المدير العام</title>
+    <link rel="stylesheet" type="text/css" href="style20.css">   
+</head>
+<body bgcolor="#ffddf7">
+    <center>
+        <font color='a83300'>
+
+    <div class="login-box">
+        <center></center>
+        <br>
+        <br>
+            <form>
+                <font color='2b001a'>
+                   <br>
+                   <br>
+                    <form>
+                        <img src="f77.png" class="avatar">
+                    <h1>  -: شـــاشةالمديـر العام </h1>
+                    </form>
+                <center>
+                    <button>
+                        <a href="index7.php"target="-blank"><h2>تسجيل بيانات المركــز</h2></a>
+                        
+                        <button>
+                            
+                            <a href="index2.php"target="-blank"><h2>أضــــافـة المستخدم</h2></a>
+                            </button>
+
+                            <br>
+                            <br>
+
+                            
+                            <button>
+                                <a href="index5.php"target="-blank"><h2> عـرض التقارير</h2></a>
+                                
+                            <button>
+                                <a href="index9.php"target="-blank"><h2> أدراة المستخدمـين</h2></a>
+                
+                            </button>
+
+                            
+                            <br>
+                            <br> 
+                        <br>
+                        <button>
+                            <a href="index12.php"target="-blank"><h2> للـرجوع الا شاشة التسجيل من جديد</h2></a>
+                            </button>
+
+                            <br>
+                            <br>   
+
+
+                </center>
+               
+
+            </form>
+        
+        
+        </div>
+    </center>
+
+              
+
+    </body>
+</html>
---
+++ index3.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<! DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewpor" content=" width=device-width, initial-scale=1.8">
+    <title>   </title>
+    <link rel="stylesheet" type="text/css" href="style3.css">   
+</head>
+<body>
+        <center> 
+    <img src="f77.png" class="avatar">
+                
+    <button>
+      
+        <a href="index20.php"target="-blank"><p>الرجوع الا شاشة المدير</p></a> 
+        </button>
+    </center>
+
+
+    </body>
+</html>
